3ba1edad5ecb8d29c692e641cbdf9154986c5407
Christopher Campbell <luthrian@gmail.com>
2018-03-10 00:48:08 +0100

    coding ongoing ...

--- src/main/java/com/camsolute/code/camp/engine/camunda/event/SendMessageTaskListener.java
+++ src/main/java/com/camsolute/code/camp/engine/camunda/event/SendMessageTaskListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2017 Christopher Campbell (campbellccc@gmail.com)
+ * Copyright (C) 2018 Christopher Campbell (campbellccc@gmail.com)
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
